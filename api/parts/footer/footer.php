c965bd6254b4ac1598277230267dbef503982f04
Harsh-Rathore-simtech <harsh.rathore@simtechitsolutions.in>
2026-02-04 15:49:22 +0530

    chore: comment out the About Us link in the footer.

--- api/parts/footer/footer.php
+++ api/parts/footer/footer.php
@@ -40,7 +40,7 @@
                                 <div class="footer-widget__links-box">
                                     <ul>
                                         <li><a href="index.php">Home</a></li>
-                                        <li><a href="about.php">About Us</a></li>
+                                        <!-- <li><a href="about.php">About Us</a></li> -->
 
                                         <li><a href="contact.php">Contact Us</a></li>
                                         <li><a href="blog.php">Our Blog</a></li>
